e9240d943131c91082823ab8616936ba1aff83c6
Marcelo Ramalho <marceloramalho.info@gmail.com>
2021-06-11 19:35:02 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 window.addEventListener("scroll", function(){
 	var sectionhome = document.querySelector("#section-home");
-    var sectionprojetos = document.querySelector("#projetos");
+    var sectionprojetos = document.querySelector("#websitespessoais-section");
 	sectionhome.classList.toggle("scrolado", window.scrollY > 400)
     
 	sectionprojetos.classList.toggle("scrolado-projetos", window.scrollY > window.innerHeight * 1.3)
 
-
+	
     console.log(scrollY)
 })
 
