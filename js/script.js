c6d91953c59b9a1fb4591b378e9cd9bb130d6085
Marcelo Ramalho <marceloramalho.info@gmail.com>
2021-06-12 01:28:16 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,48 @@
-window.addEventListener("scroll", function(){
+
+const navbarSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.navbar-links')
+	const navLinks = document.querySelectorAll('.navbar-links li');
+	const navLinksA = document.querySelectorAll('.barra-item');
+
+
+	burger.addEventListener('click', () => {
+		//Toggle Nav
+		nav.classList.toggle('navbar-ativo');
+		//Animate Links
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+			}
+		});
+		//Burger Animation
+		burger.classList.toggle('toggle')
+	});
+	
+	for (let i = 0; i < navLinksA.length; i++) {
+		navLinksA[i].addEventListener("click", function() {
+			//console.log(i)
+			nav.classList.toggle('navbar-ativo');
+		//Animate Links
+		navLinks.forEach((link) => {
+			link.style.animation = '';
+		});
+		});
+	}
+
+
+}
+navbarSlide();
+
+window.addEventListener("scroll", function () {
 	var sectionhome = document.querySelector("#section-home");
-    var sectionprojetos = document.querySelector("#websitespessoais-section");
+	var sectionprojetos = document.querySelector("#websitespessoais-section");
 	sectionhome.classList.toggle("scrolado", window.scrollY > 400)
-    
+
 	sectionprojetos.classList.toggle("scrolado-projetos", window.scrollY > window.innerHeight * 1.3)
 
-	
-    console.log(scrollY)
-})
 
+	//console.log(scrollY)
+})
